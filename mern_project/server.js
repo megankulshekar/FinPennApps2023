9eb90e0c737a03c1249f12815636efc290a143c2
megankulshekar <91085343+megankulshekar@users.noreply.github.com>
2023-09-10 00:02:21 -0400

    changed files

---
+++ mern_project/server.js
@@ -0,0 +1,31 @@
+// server.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/mern_app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Define routes (API endpoints)
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
